7d03202587035781929a2cec5f7274a0e57a906a
Евгений Усков <zhek@macbook-pro.(none)>
2012-01-24 15:36:34 +0400

    https://www.pivotaltracker.com/story/show/23872267

--- spec/javascripts/filter_data_spec.js
+++ spec/javascripts/filter_data_spec.js
@@ -1,8 +1,8 @@
 describe("Filter data", function(){
 
-  var tickets, tickets2, felter_result, felter_result2;
+  var tickets_filter_data, tickets_filter_data2, felter_result, felter_result2;
 
-  tickets = [
+  tickets_filter_data = [
     {
       direct_flight_time: 1327440000,
       return_flight_time: 1327565700,
@@ -58,7 +58,7 @@ describe("Filter data", function(){
     }
   ];
 
-  tickets2 = [
+  tickets_filter_data2 = [
     {
       direct_flight_time: 1327440000,
       return_flight_time: 1327565700,
@@ -144,17 +144,24 @@ describe("Filter data", function(){
   };
 
 
+  _.each(_.range(98), function (){
+    tickets_filter_data.push(tickets_filter_data[0]);
+    tickets_filter_data.push(tickets_filter_data[1]);
+    tickets_filter_data.push(tickets_filter_data[2]);
+    tickets_filter_data.push(tickets_filter_data[3]);
+  });
+
 
   it("make filter data", function(){
     runs(function(){
-      var filter_data = FilterData(tickets);
+      var filter_data = FilterData(tickets_filter_data);
       expect(filter_data).toEqual(felter_result);
     });
   });
 
   it("make filter data (identical price)", function(){
     runs(function(){
-      var filter_data = FilterData(tickets2);
+      var filter_data = FilterData(tickets_filter_data2);
       expect(filter_data).toEqual(felter_result2);
     });
   });
