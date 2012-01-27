980cfcbe29dc98e025b6551a7031ccaff0b86294
aki <akimoto@yandex.ru>
2012-01-27 19:38:54 +0400

    levenstain length appended

--- spec/javascripts/dictionary_spec.js
+++ spec/javascripts/dictionary_spec.js
@@ -1,9 +1,33 @@
 describe("fuzzy dictionary", function(){
+  var build_start = +new Date();
   var test_dict = NANO.dictionary.create("test", dictionary_test_data);
+  var build_end = +new Date();
+  var start = +new Date();
+  var end = null;
+
+  var items_count = dictionary_test_data.length;
+
   waitsFor(function(){
-    return test_dict.ready;
+    if(test_dict.ready){
+      end = +new Date();
+      return true;
+    }
+    return false;
   });
-  it("strict search", function(){});
+  it("dictionary init time", function(){
+    runs(function(){
+      var init_time = end - start;
+      var build_time = build_end - build_start;
+      expect(init_time).toBeLessThan(items_count * 5);
+    });
+  });
+  it("strict search", function(){
+    runs(function(){
+      var data = test_dict.strictly("Масква");
+      console.log(data);
+      // expect(init_time).toBeLessThan(items_count * 5);
+    });
+  });
   it("soft search", function(){});
   it("search by iata", function(){});
   it("get iata info", function(){});
