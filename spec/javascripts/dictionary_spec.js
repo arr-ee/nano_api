5141c1b329acfae405be871f9dc70f9bc514d4b4
aki <akimoto@yandex.ru>
2012-01-30 11:20:44 +0400

    optimisations, bugfixes and tests for dictionary

--- spec/javascripts/dictionary_spec.js
+++ spec/javascripts/dictionary_spec.js
@@ -3,17 +3,21 @@ describe("fuzzy dictionary", function(){
   var test_dict = NANO.dictionary.create("test", dictionary_test_data);
   var build_end = +new Date();
   var start = +new Date();
-  var end = null;
+  var end = 0;
 
   var items_count = dictionary_test_data.length;
 
-  waitsFor(function(){
-    if(test_dict.ready){
-      end = +new Date();
-      return true;
-    }
-    return false;
+  beforeEach(function(){
+    waitsFor(function(){
+      if(test_dict.ready){
+        end = +new Date();
+        return true;
+      }
+      return false;
+    });
   });
+  
+
   it("dictionary init time", function(){
     runs(function(){
       var init_time = end - start;
@@ -21,14 +25,31 @@ describe("fuzzy dictionary", function(){
       expect(init_time).toBeLessThan(items_count * 5);
     });
   });
-  it("strict search", function(){
+  it("strict search with results", function(){
     runs(function(){
       var data = test_dict.strictly("Масква");
-      console.log(data);
-      // expect(init_time).toBeLessThan(items_count * 5);
+      expect(data[0].iata).toBe("MOW");
+      expect(data[0].name).toBe("Москва, Россия");
     });
   });
-  it("soft search", function(){});
+  it("strict search with no results", function(){
+    runs(function(){
+      var data = test_dict.strictly("Маскваasdjh");
+      expect(data.length).toBe(0);
+    });
+  });
+  it("soft search", function(){
+    runs(function(){
+      var data = test_dict.softly("Хаб"); // 
+      var iatas = _.map(data, function(place_data){
+        return place_data.iata;
+      });
+      expect(iatas).toContain("KHV");
+      expect(iatas).toContain("HRK");
+      expect(iatas).toContain("CHQ");
+      expect(iatas).toContain("HFA");
+    });
+  });
   it("search by iata", function(){});
   it("get iata info", function(){});
   it("get city info", function(){});
