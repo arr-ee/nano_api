593c419a30d0f3b98c7fa1fdade1a4cfdb078a45
aki <akimoto@yandex.ru>
2012-02-16 14:28:57 +0400

    getting true search results

--- app/controllers/nano_api/application_controller.rb
+++ app/controllers/nano_api/application_controller.rb
@@ -1,5 +1,7 @@
 module NanoApi
   class ApplicationController < ::ApplicationController
+    skip_before_filter :verify_authenticity_token
+    
     protected
     
     def forward_json json
--- app/controllers/nano_api/searches_controller.rb
+++ app/controllers/nano_api/searches_controller.rb
@@ -6,15 +6,8 @@ module NanoApi
     end
 
     def create
-      search_result = Client.search('my_additional_marker',
-        origin_iata: 'MOW',
-        destination_iata: 'BKK',
-        depart_date: Date.new(2012, 3, 20),
-        return_date: Date.new(2012, 4, 20),
-        adults: 2,
-        children: 1,
-        trip_class: 0
-      )
+      search_params = params.reject{|key, value| %w[action controller format].include? key}
+      search_result = Client.search('my_additional_marker', search_params)
 
       if search_result.present?
         forward_json search_result
