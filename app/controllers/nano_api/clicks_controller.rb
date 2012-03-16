b7f3ab2dfa156d99cbd4b1d0d2ad023b07646ad4
Igor Vechkanov <xxxvii@mail.ru>
2012-03-16 20:47:44 +0700

    Добавил стриминг в контроллер кликов.

--- app/controllers/nano_api/clicks_controller.rb
+++ app/controllers/nano_api/clicks_controller.rb
@@ -2,7 +2,7 @@ module NanoApi
   class ClicksController < ApplicationController
     def new
       if @click_form = Client.click(params[:search_id], params[:url_id])
-        render layout: false
+        render stream: true
       else
         redirect_to new_searches_url
       end
