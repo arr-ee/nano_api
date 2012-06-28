5cf2904adaa1a1be4e7beabbe399021ff43bd9d0
pyromaniac <kinwizard@gmail.com>
2012-06-28 18:16:48 +0700

    Added smart date parsing

--- lib/nano_api/model/extensions/date.rb
+++ lib/nano_api/model/extensions/date.rb
@@ -10,7 +10,31 @@ module NanoApi
 
         module ClassMethods
           def modelize value
-            parse(value.to_s) rescue nil
+            smart_parse(value.to_s) rescue nil
+          end
+
+          def smart_parse date_or_string, fallback = nil
+            fallback_value = fallback.respond_to?(:call) ? fallback.call : fallback
+            date = case date_or_string
+                   when String
+                     string = date_or_string.strip.gsub(/\s+/, '.').mb_chars.downcase
+                     begin
+                       string.to_date
+                     rescue TypeError, NoMethodError
+                       I18n.t('date.month_names').each_with_index do |month, index|
+                         next if month.nil?
+                         if string.include?(month)
+                           string.gsub!(month, index.to_s.rjust(2, '0'))
+                         end
+                       end
+
+                       string.to_date rescue fallback_value
+                     end
+                   else
+                     date_or_string
+                   end
+
+            date or fallback_value
           end
         end
       end
