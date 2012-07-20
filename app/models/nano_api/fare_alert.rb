e17408219b4c10e96a45cc898639515afc1bd470
pyromaniac <kinwizard@gmail.com>
2012-07-20 19:37:36 +0700

    Fixed fare alert

--- app/models/nano_api/fare_alert.rb
+++ app/models/nano_api/fare_alert.rb
@@ -2,8 +2,10 @@ module NanoApi
   class FareAlert
     include NanoApi::Model
 
-    attribute :origin_hash, type: Hash
-    attribute :destination_hash, type: Hash
+    attribute :origin_code
+    attribute :origin_name
+    attribute :destination_code
+    attribute :destination_name
     attribute :depart_date, type: Date
     attribute :return_date, type: Date
   end
