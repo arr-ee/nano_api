b4c5207ed8598d0d9d4bb45c0acd13bb2f13bab6
pyromaniac <kinwizard@gmail.com>
2012-08-22 14:40:56 +0700

    Added locale mappings

--- lib/nano_api/client/places.rb
+++ lib/nano_api/client/places.rb
@@ -1,12 +1,16 @@
 module NanoApi::Client::Places
 
+  MAPPING = {:'zh-CN' => :cn}
+
   def auto_complete_place term, locale = I18n.locale
+    locale = MAPPING[locale] || locale
     get_raw('places_%s' % locale, term: term)
   rescue RestClient::BadRequest
     nil
   end
 
   def geoip ip, locale = I18n.locale
+    locale = MAPPING[locale] || locale
     get('places/ip_to_places_%s' % locale, ip: ip).first.try(:symbolize_keys)
   end
 
