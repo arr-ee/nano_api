b9c2624f67a7f206316bd506aa2e4a9b1d3d60c1
Andrew Gridnev <andrew.gridnev@gmail.com>
2012-09-06 14:40:38 +0800

    nano_api_search_iatas fabricator unsets cached dest, orig names after creating an instance

--- spec/fabricators/nano_api_search_fabricator.rb
+++ spec/fabricators/nano_api_search_fabricator.rb
@@ -6,4 +6,9 @@ end
 Fabricator :nano_api_search_iatas, :from => 'nano_api/search' do
   origin_iata 'MOW'
   destination_iata 'LON'
+
+  after_create do |search|
+  	search.instance_eval {remove_instance_variable("@destination_name_default")}
+  	search.instance_eval {remove_instance_variable("@origin_name_default")}
+  end
 end
