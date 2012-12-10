33b0c40257d4a33733a84f89b1f2a2badb62cfa6
pyromaniac <kinwizard@gmail.com>
2012-12-10 20:54:27 +0700

    [#40931063] Uniq clicks

--- lib/nano_api/backends/controllers/clicks_controller.rb
+++ lib/nano_api/backends/controllers/clicks_controller.rb
@@ -11,4 +11,25 @@ class NanoApi::Backends::ClicksController < NanoApi::ApplicationController
       redirect_to new_search_url
     end
   end
+
+private
+
+  def click_params
+    NanoApi.client.click(params[:search_id], params[:id],
+      {:unique => _uniq_click?('c')})
+  end
+
+  def _uniq_click? scope, gate_id = _gate_id
+    name = :"uc#{scope}#{gate_id}"
+    clicked = cookies[name].present?
+    cookies[name] = {
+      :value => 1,
+      :expires => 1.day.from_now
+    } unless clicked
+    clicked ? '0' : '1'
+  end
+
+  def _gate_id
+    params[:gate_id]
+  end
 end
--- lib/nano_api/client/click.rb
+++ lib/nano_api/client/click.rb
@@ -1,8 +1,8 @@
 module NanoApi
   module Client
     module Click
-      def click search_id, order_url_id
-        post('searches/%d/order_urls/%d' % [search_id, order_url_id]).symbolize_keys
+      def click search_id, order_url_id, params = {}
+        post('searches/%d/order_urls/%d' % [search_id, order_url_id], params).symbolize_keys
       rescue RestClient::ResourceNotFound
         nil
       end
